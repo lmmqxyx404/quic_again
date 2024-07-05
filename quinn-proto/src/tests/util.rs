6cd43248c84195d0ed5d9574e2a6b6a19ef59ca2
lmmqxyx <1368470804@qq.com>
2024-07-06 00:21:10 +0800

    to impl client_crypto_inner deal with roots tag4

--- quinn-proto/src/tests/util.rs
+++ quinn-proto/src/tests/util.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use rustls::pki_types::CertificateDer;
+use lazy_static::lazy_static;
 
 use crate::{config::ClientConfig, crypto::rustls::QuicClientConfig};
 
@@ -16,5 +17,17 @@ fn client_crypto_inner(
     certs: Option<Vec<CertificateDer<'static>>>,
     alpn: Option<Vec<Vec<u8>>>,
 ) -> QuicClientConfig {
+    let mut roots = rustls::RootCertStore::empty();
+    for cert in certs.unwrap_or_else(|| vec![CERTIFIED_KEY.cert.der().clone()]) {
+        roots.add(cert).unwrap();
+    }
+
     todo!()
+}
+
+
+lazy_static! {
+    
+    pub(crate) static ref CERTIFIED_KEY: rcgen::CertifiedKey =
+        rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
 }
