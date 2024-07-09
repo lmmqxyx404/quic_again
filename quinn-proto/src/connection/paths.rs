e7a75ca3b1b29ecb5835e973bfb34f0671e51db7
lmmqxyx <1368470804@qq.com>
2024-07-10 01:29:36 +0800

    continue version_negotiate_client client_ch.handle_event optimize Connection::new tag10

--- quinn-proto/src/connection/paths.rs
+++ quinn-proto/src/connection/paths.rs
@@ -1,7 +1,21 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Instant};
+
+use crate::endpoint::TransportConfig;
 
 /// Description of a particular network path
 pub(super) struct PathData {
-  pub(super) remote: SocketAddr,
+    pub(super) remote: SocketAddr,
+}
 
+impl PathData {
+    pub(super) fn new(
+        remote: SocketAddr,
+        allow_mtud: bool,
+        peer_max_udp_payload_size: Option<u16>,
+        now: Instant,
+        validated: bool,
+        config: &TransportConfig,
+    ) -> Self {
+        Self { remote }
+    }
 }
