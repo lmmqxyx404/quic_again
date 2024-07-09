17962bab25d5202dd968e4021a76ddfe48039c00
lmmqxyx <1368470804@qq.com>
2024-07-10 03:52:45 +0800

    Connection::new crypto.initial_keys(&init_cid, side) complete fn initial_keys tag2

--- quinn-proto/src/crypto/rustls.rs
+++ quinn-proto/src/crypto/rustls.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
 
+use bytes::BytesMut;
 use rustls::{
     client::danger::ServerCertVerifier,
     pki_types::ServerName,
-    quic::{Connection, Secrets, Suite, Version},
+    quic::{Connection, HeaderProtectionKey, PacketKey, Secrets, Suite, Version},
 };
 
 use crate::{
@@ -11,7 +12,7 @@ use crate::{
     transport_parameters::TransportParameters, Side,
 };
 
-use super::{Keys, UnsupportedVersion};
+use super::{CryptoError, KeyPair, Keys, UnsupportedVersion};
 
 impl From<Side> for rustls::Side {
     fn from(s: Side) -> Self {
@@ -166,5 +167,79 @@ pub(crate) fn initial_keys(
 ) -> Keys {
     let keys = suite.keys(dst_cid, side.into(), version);
 
-    todo!()
+    Keys {
+        header: KeyPair {
+            local: Box::new(keys.local.header),
+            remote: Box::new(keys.remote.header),
+        },
+        packet: KeyPair {
+            local: Box::new(keys.local.packet),
+            remote: Box::new(keys.remote.packet),
+        },
+    }
+}
+
+impl crypto::HeaderKey for Box<dyn HeaderProtectionKey> {
+    fn decrypt(&self, pn_offset: usize, packet: &mut [u8]) {
+        let (header, sample) = packet.split_at_mut(pn_offset + 4);
+        let (first, rest) = header.split_at_mut(1);
+        let pn_end = Ord::min(pn_offset + 3, rest.len());
+        self.decrypt_in_place(
+            &sample[..self.sample_size()],
+            &mut first[0],
+            &mut rest[pn_offset - 1..pn_end],
+        )
+        .unwrap();
+    }
+
+    fn encrypt(&self, pn_offset: usize, packet: &mut [u8]) {
+        let (header, sample) = packet.split_at_mut(pn_offset + 4);
+        let (first, rest) = header.split_at_mut(1);
+        let pn_end = Ord::min(pn_offset + 3, rest.len());
+        self.encrypt_in_place(
+            &sample[..self.sample_size()],
+            &mut first[0],
+            &mut rest[pn_offset - 1..pn_end],
+        )
+        .unwrap();
+    }
+
+    fn sample_size(&self) -> usize {
+        self.sample_len()
+    }
+}
+
+impl crypto::PacketKey for Box<dyn PacketKey> {
+    fn encrypt(&self, packet: u64, buf: &mut [u8], header_len: usize) {
+        let (header, payload_tag) = buf.split_at_mut(header_len);
+        let (payload, tag_storage) = payload_tag.split_at_mut(payload_tag.len() - self.tag_len());
+        let tag = self.encrypt_in_place(packet, &*header, payload).unwrap();
+        tag_storage.copy_from_slice(tag.as_ref());
+    }
+
+    fn decrypt(
+        &self,
+        packet: u64,
+        header: &[u8],
+        payload: &mut BytesMut,
+    ) -> Result<(), CryptoError> {
+        let plain = self
+            .decrypt_in_place(packet, header, payload.as_mut())
+            .map_err(|_| CryptoError)?;
+        let plain_len = plain.len();
+        payload.truncate(plain_len);
+        Ok(())
+    }
+
+    fn tag_len(&self) -> usize {
+        (**self).tag_len()
+    }
+
+    fn confidentiality_limit(&self) -> u64 {
+        (**self).confidentiality_limit()
+    }
+
+    fn integrity_limit(&self) -> u64 {
+        (**self).integrity_limit()
+    }
 }
