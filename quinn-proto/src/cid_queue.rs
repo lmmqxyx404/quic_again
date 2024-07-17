acd5ab1ed3160355fc87ab42aa176e328b11d098
lmmqxyx <1368470804@qq.com>
2024-07-17 09:36:47 +0800

    optimize CidQueue::new active

--- quinn-proto/src/cid_queue.rs
+++ quinn-proto/src/cid_queue.rs
@@ -1,19 +1,29 @@
-use crate::shared::ConnectionId;
+use crate::{shared::ConnectionId, token::ResetToken};
+
+/// DataType stored in CidQueue buffer
+type CidData = (ConnectionId, Option<ResetToken>);
 
 /// Sliding window of active Connection IDs
 ///
 /// May contain gaps due to packet loss or reordering
 #[derive(Debug)]
-pub(crate) struct CidQueue {}
+pub(crate) struct CidQueue {
+    /// 1. Ring buffer indexed by `self.cursor`
+    buffer: [Option<CidData>; Self::LEN],
+    /// 2. Index at which circular buffer addressing is based
+    cursor: usize,
+}
 
 impl CidQueue {
+    pub(crate) const LEN: usize = 5;
+    /// 1.
     pub(crate) fn new(cid: ConnectionId) -> Self {
-        Self {}
+        let mut buffer = [None; Self::LEN];
+        buffer[0] = Some((cid, None));
+        Self { buffer, cursor: 0 }
     }
-
-    /// Return active remote CID itself
+    /// 2.Return active remote CID itself
     pub(crate) fn active(&self) -> ConnectionId {
-        todo!()
-        // self.buffer[self.cursor].unwrap().0
+        self.buffer[self.cursor].unwrap().0
     }
 }
