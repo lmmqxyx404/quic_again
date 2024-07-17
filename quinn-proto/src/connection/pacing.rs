b3b85d7d3d0eae98e918b97ef8f9129d5aa9a53e
lmmqxyx <1368470804@qq.com>
2024-07-17 10:59:33 +0800

    complete Pacer::new

--- quinn-proto/src/connection/pacing.rs
+++ quinn-proto/src/connection/pacing.rs
@@ -13,7 +13,8 @@ pub(super) struct Pacer {}
 impl Pacer {
     /// 1. Obtains a new [`Pacer`].
     pub(super) fn new(smoothed_rtt: Duration, window: u64, mtu: u16, now: Instant) -> Self {
-        todo!()
+        let capacity = optimal_capacity(smoothed_rtt, window, mtu);
+        Self {}
     }
 
     /// 2. Return how long we need to wait before sending `bytes_to_send`
@@ -34,3 +35,40 @@ impl Pacer {
         todo!()
     }
 }
+
+/// Calculates a pacer capacity for a certain window and RTT
+///
+/// The goal is to emit a burst (of size `capacity`) in timer intervals
+/// which compromise between
+/// - ideally distributing datagrams over time
+/// - constantly waking up the connection to produce additional datagrams
+///
+/// Too short burst intervals means we will never meet them since the timer
+/// accuracy in user-space is not high enough. If we miss the interval by more
+/// than 25%, we will lose that part of the congestion window since no additional
+/// tokens for the extra-elapsed time can be stored.
+///
+/// Too long burst intervals make pacing less effective.
+fn optimal_capacity(smoothed_rtt: Duration, window: u64, mtu: u16) -> u64 {
+    let rtt = smoothed_rtt.as_nanos().max(1);
+
+    let capacity = ((window as u128 * BURST_INTERVAL_NANOS) / rtt) as u64;
+
+    // Small bursts are less efficient (no GSO), could increase latency and don't effectively
+    // use the channel's buffer capacity. Large bursts might block the connection on sending.
+    capacity.clamp(MIN_BURST_SIZE * mtu as u64, MAX_BURST_SIZE * mtu as u64)
+}
+
+/// The burst interval
+///
+/// The capacity will we refilled in 4/5 of that time.
+/// 2ms is chosen here since framework timers might have 1ms precision.
+/// If kernel-level pacing is supported later a higher time here might be
+/// more applicable.
+const BURST_INTERVAL_NANOS: u128 = 2_000_000; // 2ms
+
+/// Allows some usage of GSO, and doesn't slow down the handshake.
+const MIN_BURST_SIZE: u64 = 10;
+
+/// Creating 256 packets took 1ms in a benchmark, so larger bursts don't make sense.
+const MAX_BURST_SIZE: u64 = 256;
