27fa8c5c5892847913a93c32ef2d55f5345f0e26
lmmqxyx <1368470804@qq.com>
2024-07-22 22:37:26 +0800

    impl DatagramState::write returned false tag1

--- quinn-proto/src/connection/datagrams.rs
+++ quinn-proto/src/connection/datagrams.rs
@@ -16,6 +16,10 @@ impl DatagramState {
     /// Returns whether a frame was written. At most `max_size` bytes will be written, including
     /// framing.
     pub(super) fn write(&mut self, buf: &mut Vec<u8>, max_size: usize) -> bool {
+        let datagram = match self.outgoing.pop_front() {
+            Some(x) => x,
+            None => return false,
+        };
         todo!()
     }
 }
