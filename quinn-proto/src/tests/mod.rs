d8b683faf19af78e92f8d904e553263ff4e75c72
lmmqxyx <1368470804@qq.com>
2024-08-07 22:38:07 +0800

    impl tests::packet_splitting_not_necessary_after_higher_mtu_discovered tag1

--- quinn-proto/src/tests/mod.rs
+++ quinn-proto/src/tests/mod.rs
@@ -2416,6 +2416,27 @@ fn packet_splitting_with_default_mtu() {
     assert_eq!(pair.server.inbound.len(), 2);
 }
 
+#[test]
+fn packet_splitting_not_necessary_after_higher_mtu_discovered() {
+    let _guard = subscribe();
+    let payload = vec![42; 1300];
+
+    let mut pair = Pair::default();
+    pair.mtu = 1500;
+
+    let (client_ch, _) = pair.connect();
+    pair.drive();
+
+    let s = pair.client_streams(client_ch).open(Dir::Uni).unwrap();
+
+    pair.client_send(client_ch, s).write(&payload).unwrap();
+    pair.client.drive(pair.time, pair.server.addr);
+    assert_eq!(pair.client.outbound.len(), 1);
+
+    pair.drive_client();
+    assert_eq!(pair.server.inbound.len(), 1);
+}
+
 fn stream_chunks(mut recv: RecvStream) -> Vec<u8> {
     let mut buf = Vec::new();
 
