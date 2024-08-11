d39bf244ed0a3751e086d5253d75f633be3b5a24
lmmqxyx <1368470804@qq.com>
2024-08-11 22:26:50 +0800

    add a new struct UdpSockRef

--- quinn-udp/src/lib.rs
+++ quinn-udp/src/lib.rs
@@ -1,5 +1,25 @@
 #[cfg(unix)]
+use std::os::unix::io::AsFd;
+
+#[cfg(unix)]
 #[path = "unix.rs"]
 mod imp;
 
 pub use imp::UdpSocketState;
+
+/// A borrowed UDP socket
+///
+/// On Unix, constructible via `From<T: AsFd>`. On Windows, constructible via `From<T:
+/// AsSocket>`.
+// Wrapper around socket2 to avoid making it a public dependency and incurring stability risk
+pub struct UdpSockRef<'a>(socket2::SockRef<'a>);
+
+#[cfg(unix)]
+impl<'s, S> From<&'s S> for UdpSockRef<'s>
+where
+    S: AsFd,
+{
+    fn from(socket: &'s S) -> Self {
+        Self(socket.into())
+    }
+}
--- quinn-udp/src/unix.rs
+++ quinn-udp/src/unix.rs
@@ -1,6 +1,16 @@
+use std::io;
+
+use crate::UdpSockRef;
+
 /// Tokio-compatible UDP socket with some useful specializations.
 ///
 /// Unlike a standard tokio UDP socket, this allows ECN bits to be read and written on some
 /// platforms.
 #[derive(Debug)]
 pub struct UdpSocketState {}
+
+impl UdpSocketState {
+    pub fn new(sock: UdpSockRef<'_>) -> io::Result<Self> {
+        todo!()
+    }
+}
