f930f2fca115e766e731909eeac9bb19ff3c7223
lmmqxyx <1368470804@qq.com>
2024-09-04 16:43:21 +0800

    impl Endpoint::connect_with refactor Future for Connecting  tag4

--- README_QUINN.md
+++ README_QUINN.md
@@ -3,3 +3,6 @@
 注意引入方式与使用方式，在`tests::RootCertStore`
 
 ### 注意`features.rustls`之后添加的依赖项
+
+## 2. `connection.rs` 之中的 `Connection`
+这个模块中会自行定义一个`struct Connection`
--- quinn/src/connection.rs
+++ quinn/src/connection.rs
@@ -1,10 +1,11 @@
 use std::{
     future::Future,
     pin::Pin,
+    sync::Arc,
     task::{Context, Poll},
 };
 
-use proto::{Connection, ConnectionError};
+use proto::ConnectionError;
 
 /// In-progress connection attempt future
 /// todo: tag
@@ -18,3 +19,28 @@ impl Future for Connecting {
         todo!()
     }
 }
+
+/// A QUIC connection.
+///
+/// If all references to a connection (including every clone of the `Connection` handle, streams of
+/// incoming streams, and the various stream types) have been dropped, then the connection will be
+/// automatically closed with an `error_code` of 0 and an empty `reason`. You can also close the
+/// connection explicitly by calling [`Connection::close()`].
+///
+/// May be cloned to obtain another handle to the same connection.
+///
+/// [`Connection::close()`]: Connection::close
+#[derive(Debug, Clone)]
+pub struct Connection(ConnectionRef);
+
+#[derive(Debug)]
+pub(crate) struct ConnectionRef(Arc<ConnectionInner>);
+
+impl Clone for ConnectionRef {
+    fn clone(&self) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct ConnectionInner {}
